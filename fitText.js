62ecdf512b8530aadee8dd2db7ad2deff596d603
Azeem <99657466+sa-azeem@users.noreply.github.com>
2024-04-04 14:45:31 +0530

    Update fitText.js

--- fitText.js
+++ fitText.js
@@ -1,54 +1,60 @@
-const fitText=(s, x, y, wi, he)=>{
-  original_rectMode=this._renderer._rectMode
-  textAlign(CENTER,CENTER)
-  const [fontSize, lines] = getFont(wi, he, s)
-  push()
+const fitText = (s, x, y, wi, he) => {
+  const getFont = (wi, he, s) => {
+    const getMaxFontSize = (he) => {
+      let fontSize = 1;
+      let textHeight;
+      do {
+        textSize(fontSize);
+        textHeight = textAscent() + textDescent();
+        fontSize++;
+      } while (textHeight < he);
+      return fontSize;
+    };
+    const getLongestLine = (l) => {
+      let longestLine = l[0];
+      for (let i = 1; i < l.length; i++) {
+        if (l[i].length <= longestLine.length) continue;
+        longestLine = l[i];
+      }
+      return longestLine;
+    };
+    let maxFontSize = getMaxFontSize(he);
+    let fontSize = maxFontSize;
+    let lines = [s];
+    let words = s.split(" ");
+    let count = 1;
+    let flag = true;
+    while (flag) {
+      textSize(fontSize);
+      let maxLineLength = getLongestLine(lines);
+      let maxLineWidth = textWidth(maxLineLength);
+      if (maxLineWidth < wi) break;
+      count++;
+      lines = [];
+      let wordsCount = words.length;
+      let noOfLines = count;
+      if (count > wordsCount) noOfLines = wordsCount;
+      for (let j = 0; j < noOfLines; j++) {
+        lines[j] = words
+          .slice(
+            ((wordsCount * j) / noOfLines) | 0,
+            ((wordsCount * (j + 1)) / noOfLines) | 0
+          )
+          .join(" ");
+      }
+      fontSize = fontSize * 0.5;
+      if (fontSize <= 1) flag = false;
+    }
+    return [fontSize, lines];
+  };
+  current_rectMode = this._renderer._rectMode;
+  textAlign(CENTER, CENTER);
+  const [fontSize, lines] = getFont(wi, he, s);
+  push();
   textSize(fontSize);
-  if(original_rectMode==='corner')translate(x+wi/2, y+he/2);
+  if (current_rectMode === "corner") translate(x + wi / 2, y + he / 2);
   else translate(x, y);
   text(lines.join("\n"), 0, 0);
-  pop()
-}
-const getFont = (wi, he, s) => {
-  const getMaxFontSize = (he) => {
-    let fz = 1;
-    let tH;
-    do {
-      textSize(fz);
-      tH = textAscent() + textDescent();
-      fz++;
-    } while (tH < he);
-    return fz;
-  };
-  const getLongestLine = (l) => {
-    let lin = l[0];
-    for (let i = 1; i < l.length; i++) {
-      if (l[i].length <= lin.length) continue;
-      lin = l[i];
-    }
-    return lin;
-  };
-  let maxFontSize = getMaxFontSize(he);
-  let fz = maxFontSize;
-  let lines = [s];
-  let words = s.split(" ");
-  let n = 1;
-  let flag = true;
-  while (flag) {
-    textSize(fz);
-    let maxLineLength = getLongestLine(lines);
-    let tw = textWidth(maxLineLength);
-    if (tw < wi) break;
-    n++;
-    lines = [];
-    for (let j = 0; j < n; j++) {
-      let m = words.length;
-      lines[j] = words
-        .slice(((m * j) / n) | 0, ((m * (j + 1)) / n) | 0)
-        .join(" ");
-    }
-    fz = fz*.5;
-    if (fz <= 1) flag = false;
-  }
-  return [fz, lines];
+  pop();
+  return [fontSize,lines]
 };
